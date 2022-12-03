a879d37b6031cfc4a966e511b0fcf1b92d2e4e8e
Kris Laratta <krislaratta@gmail.com>
2022-12-03 12:42:17 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kris Laratta on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
